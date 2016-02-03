55103529fd5e1b25ed6468ed6aace8139341227f
Cory McDonald <cory@corywmcdonald.com>
2016-02-02 21:57:40 -0600

    Upgrade to .NET 4.6 and newest version of MVC

---
+++ Procurable/Procurable/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Procurable.Startup))]
+namespace Procurable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
