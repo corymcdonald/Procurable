a585048939b0a8b69aa3c7fd246876209aa235ce
Cory McDonald <cory@corywmcdonald.com>
2016-02-06 13:45:12 -0600

    Add original models

---
+++ Procurable/Procurable/Models/ProjectTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Procurable.Models
+{
+    public class ProjectTask
+    {
+        public int ID { get; set; }
+        public int ProjectID { get; set; }
+        public ApplicationUser AssignedTo { get; set; }
+        public string Comments { get; set; }
+        public DateTime DateNeeded { get; set; }
+    }
+}
