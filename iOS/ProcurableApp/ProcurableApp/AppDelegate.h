b7149b98292500e4bc31f9f95b3792705ac63097
Wilson Turner <wsturner.1123@gmail.com>
2016-02-09 09:53:12 -0600

    Create Xcode project

---
+++ iOS/ProcurableApp/ProcurableApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProcurableApp
+//
+//  Created by Will Turner on 2/9/16.
+//  Copyright © 2016 Wilson Turner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/ProcurableApp/ProcurableApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProcurableApp
+//
+//  Created by Will Turner on 2/9/16.
+//  Copyright © 2016 Wilson Turner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
