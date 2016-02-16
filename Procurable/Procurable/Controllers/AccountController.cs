32ae8c0c7aedc69da89ce005cd23ec10eea2246e
Cory McDonald <cory@corywmcdonald.com>
2016-02-16 12:59:57 -0600

    Add JSON to Account Register

--- Procurable/Procurable/Controllers/AccountController.cs
+++ Procurable/Procurable/Controllers/AccountController.cs
@@ -148,25 +148,32 @@ namespace Procurable.Controllers
         [AllowAnonymous]
         public async Task<ActionResult> Register(RegisterViewModel model)
         {
+            IdentityResult result = new IdentityResult();
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
+                result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
+                    if (Request.AcceptTypes.Contains("application/json"))
+                    {
+                        return Json(new { Succeeded=true });
+                    }
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
             }
-
+            if (Request.AcceptTypes.Contains("application/json"))
+            {
+                return Json(result);
+            }
             // If we got this far, something failed, redisplay form
             return View(model);
         }
