7f6a62afd997c1d153dcd25c1d36ccb1a377ad2b
Lucas Dorrough <lucas.dorrough.b@gmail.com>
2016-03-27 23:33:46 -0500

    Automatic migration fix

--- Procurable/Procurable/Migrations/Configuration.cs
+++ Procurable/Procurable/Migrations/Configuration.cs
@@ -14,6 +14,7 @@ namespace Procurable.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
             ContextKey = "Procurable.Models.ApplicationDbContext";
         }
 
